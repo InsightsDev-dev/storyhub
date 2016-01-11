1418356944ad7c1540aacd378e21b68bf4ab8d49
chanwook <chanwook.god@gmail.com>
2016-01-11 12:40:41 +0900

    session arg resolver를 fb 프로젝트로 분리

--- src/main/java/io/storyhub/config/WebConfig.java
+++ src/main/java/io/storyhub/config/WebConfig.java
@@ -1,5 +1,6 @@
 package io.storyhub.config;
 
+import io.chanwook.facebook.UserSessionHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
